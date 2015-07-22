317a65f6cd6786f718cee968dff9fc6ebf00bc79
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-22 15:59:13 -0300

    Basic test using restify

--- api/test/api.js
+++ api/test/api.js
@@ -1,1 +1,32 @@
+'use strict'
+
+/* globals describe, before, after, it */
+
+var restify = require('restify')
 var assert = require('assert')
+var api = require('./../api')
+
+// init the test client
+var client = restify.createJsonClient({
+  url: 'http://127.0.0.1:9999',
+  version: '*'
+})
+
+describe('API routes testing', function () {
+
+  before(function () {
+    api.listen(9999)
+  })
+
+  it('should get a 200 response', function (done) {
+    client.get('/api/game', function (err, req, res, data) {
+      assert.equal(200, res.statusCode)
+      done()
+    })
+  })
+
+  after(function () {
+    // ...
+  })
+
+})
