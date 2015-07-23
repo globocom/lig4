13a52503e05582cb53c106e70c1682bc1182e6cf
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-23 17:03:47 -0300

    Game model created

--- api/models/game.js
+++ api/models/game.js
@@ -1,1 +1,17 @@
-// TODO: Create gamemodel
+'use strict'
+
+var mongoose = require('mongoose')
+var GameSchema = new mongoose.Schema({
+  winner: {
+    type: String,
+    required: true
+  },
+  board: {
+    type: Array
+  },
+  moves: {
+    type: Array
+  }
+})
+
+module.exports = mongoose.model('Game', GameSchema)
