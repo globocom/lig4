05c912d67cc38b9a6a5f35300bf674d56e52d80f
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-23 17:05:12 -0300

    First version of /api/player

--- api/handlers/player.js
+++ api/handlers/player.js
@@ -1,14 +1,73 @@
-var Player = require('./../models/player');
+'use strict'
 
+// import models
+var Player = require('./../models/player')
+var Match = require('./../models/match')
+var Leaderboard = require('./../models/leaderboard')
 
+var setupPlayer = function (data, onEnd) {
+
+  var player = new Player(data)
+  player.save(function (err) {
+
+    // Insert into leaderboard
+    var leaderboard = new Leaderboard()
+    leaderboard.player = player
+    leaderboard.score = 0
+    leaderboard.save()
+
+    // Create matches
+    Player
+      .find()
+      .where('username')
+      .ne(player.username) // each other
+      .exec(function (err, players) {
+
+        var count = 0;
+        players.forEach(function (opponent) {
+          var match = new Match()
+          match.home = player
+          match.guest = opponent
+          match.save(function (err) {
+            // OK!
+          })
+        })
+        onEnd(player)
+      })
+  })
+}
+
+// Handler for POST in /api/player
 module.exports = function playerHandler(req, res, next) {
 
-    // POST /player -> envia o codigo e a auth do github
-    // > Deve realizar o auth no github.
-    // > Testa interface (tem methodos move e done?)
-    // > Testa o player do usuario contra um dummyplayer.
-    // Salva ou retorna erro
+  // TODO: query github api
+  // TODO: server code validation
 
-  res.json(200, req.params.name)
-  return next()
+  Player
+    .findOne()
+    .where('username')
+    .equals(req.params.username)
+    .exec(function (err, player) {
+
+      if (err) {
+        res.json(500, {
+          status: 'ERROR',
+          err: err
+        })
+      } else if (player) {
+        res.json(400, {
+          status: 'ERROR',
+          err: 'PLAYER_ALREADY_EXISTS'
+        })
+      } else {
+        setupPlayer(req.params, function (newPlayer) {
+          res.json(200, {
+            status: 'OK',
+            player: newPlayer
+          })
+        })
+      }
+
+      return next()
+    })
 }
