ed5cb1bf73718e16148209f674ec5ff8c16d75e8
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-24 17:17:29 -0300

    Refactoring player handler

--- api/handlers/player.js
+++ api/handlers/player.js
@@ -2,71 +2,44 @@
 
 // import models
 var Player = require('./../models/player')
-var Match = require('./../models/match')
-var Leaderboard = require('./../models/leaderboard')
 
-var setupPlayer = function (data, onEnd) {
-
-  var player = new Player(data)
-  player.save(function (err) {
-
-    // Insert into leaderboard
-    var leaderboard = new Leaderboard()
-    leaderboard.player = player
-    leaderboard.score = 0
-    leaderboard.save()
-
-    // Create matches
-    Player
-      .find()
-      .where('username')
-      .ne(player.username) // each other
-      .exec(function (err, players) {
-
-        players.forEach(function (opponent) {
-          var match = new Match()
-          match.home = player
-          match.guest = opponent
-          match.save(function (err) {
-            // OK!
-          })
-        })
-        onEnd(player)
-      })
+function setResponse (statusCode, status, payload, res, next) {
+  res.json(statusCode, {
+    status: status,
+    payload: payload
   })
+  return next()
 }
 
 // Handler for POST in /api/player
-module.exports = function playerHandler(req, res, next) {
+function _post (req, res, next) {
 
   // TODO: query github api
   // TODO: server code validation
 
-  Player
-    .findOne()
+  var player = new Player(req.params)
+  player.save(function (err) {
+    if (err) return setResponse(500, 'ERROR', err, res, next)
+    setResponse(200, 'OK', player, res, next)
+  })
+}
+
+// Handler for GET in /api/player/:username
+function _get (req, res, next) {
+
+  // TODO: query github api
+
+  Player.findOne()
     .where('username')
     .equals(req.params.username)
     .exec(function (err, player) {
-
-      if (err) {
-        res.json(500, {
-          status: 'ERROR',
-          err: err
-        })
-      } else if (player) {
-        res.json(400, {
-          status: 'ERROR',
-          err: 'PLAYER_ALREADY_EXISTS'
-        })
-      } else {
-        setupPlayer(req.params, function (newPlayer) {
-          res.json(200, {
-            status: 'OK',
-            player: newPlayer
-          })
-        })
-      }
-
-      return next()
+      if (err) return setResponse(500, 'ERROR', err, res, next)
+      if (!player) return res.send(204)
+      setResponse(200, 'OK', player, res, next)
     })
+}
+
+module.exports = {
+  get: _get,
+  post: _post
 }
