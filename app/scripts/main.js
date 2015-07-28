c7a8e921e1b7c36eec1f692b3bd736b2da3dc7dc
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2015-07-28 15:30:53 -0300

    removed above the folde technique

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,14 +1,6 @@
 'use strict';
 
 function main () {
-  var link = document.createElement('link');
-
-  link.href = 'styles/minor.css';
-  link.media = 'none';
-  link.rel = 'stylesheet';
-  link.addEventListener('load', link.removeAttribute.bind(link, 'media'));
-
-  document.head.appendChild(link);
 };
 
 document.addEventListener('DOMContentLoaded', main);
