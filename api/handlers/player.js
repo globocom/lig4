66226ff8301c4a1f4a5547b8df6bf266e1eb187b
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-28 18:05:51 -0300

    Added PUT handler

--- api/handlers/player.js
+++ api/handlers/player.js
@@ -32,6 +32,21 @@ function _get(req, res, next) {
     })
 }
 
+// Handler for PUT in /api/player/:username
+function _put(req, res, next) {
+
+  // TODO: query github api
+
+  var data = JSON.parse(req.body)
+
+  Player.update({ username: data.username }, data, function (err, affected) {
+    if (err) return Response.send(500, 'ERROR', err, res, next)
+    if (affected === 0) return res.send(204)
+    Response.send(200, 'OK', {}, res, next)
+  })
+}
+
+
 module.exports = {
   get: _get,
   post: _post
