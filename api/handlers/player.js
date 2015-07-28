9a4f6fd5747a95c63223750bb465d340714e91a2
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-07-28 17:37:25 -0300

    Function setResponse was moved to response.js file

--- api/handlers/player.js
+++ api/handlers/player.js
@@ -2,30 +2,23 @@
 
 // import models
 var Player = require('./../models/player')
-
-function setResponse (statusCode, status, payload, res, next) {
-  res.json(statusCode, {
-    status: status,
-    payload: payload
-  })
-  return next()
-}
+var Response = require('./response')
 
 // Handler for POST in /api/player
-function _post (req, res, next) {
+function _post(req, res, next) {
 
   // TODO: query github api
   // TODO: server code validation
 
   var player = new Player(req.params)
   player.save(function (err) {
-    if (err) return setResponse(500, 'ERROR', err, res, next)
-    setResponse(200, 'OK', player, res, next)
+    if (err) return Response.send(500, 'ERROR', err, res, next)
+    Response.send(200, 'OK', player, res, next)
   })
 }
 
 // Handler for GET in /api/player/:username
-function _get (req, res, next) {
+function _get(req, res, next) {
 
   // TODO: query github api
 
@@ -33,9 +26,9 @@ function _get (req, res, next) {
     .where('username')
     .equals(req.params.username)
     .exec(function (err, player) {
-      if (err) return setResponse(500, 'ERROR', err, res, next)
+      if (err) return Response.send(500, 'ERROR', err, res, next)
       if (!player) return res.send(204)
-      setResponse(200, 'OK', player, res, next)
+      Response.send(200, 'OK', player, res, next)
     })
 }
 
