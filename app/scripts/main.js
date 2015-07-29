47c07df440131a9eb4f665c81c83992f33658315
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2015-07-29 11:51:11 -0300

    added scrollTo functionality to navigation buttons

--- app/scripts/main.js
+++ app/scripts/main.js
@@ -1,6 +1,26 @@
 'use strict';
 
+var scrollToSection = require('./libs/scroll');
+
+// elements
+var navigationButtons;
+
+// functions
+function navigationHandler (e) {
+  var button  = e.currentTarget;
+  var section = document.getElementById(button.getAttribute('data-section'));
+
+  scrollToSection(section);
+}
+
+
+// main function
 function main () {
+  navigationButtons = document.getElementsByClassName('navigation-button');
+
+  for (var i = navigationButtons.length - 1; i >= 0; i--) {
+    navigationButtons[i].addEventListener('click', navigationHandler);
+  }
 };
 
 document.addEventListener('DOMContentLoaded', main);
