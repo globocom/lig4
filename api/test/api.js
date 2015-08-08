95fd3bce22ddfe52615561e99a4251ff97b0587d
Evandro Eisinger <evandro.eisinger@corp.globo.com>
2015-08-08 17:04:31 -0300

    improved test to handle with express stack

--- api/test/api.js
+++ api/test/api.js
@@ -1,88 +1,67 @@
 'use strict'
 
-/* globals describe, before, after, it */
-
 // defaults
-process.env.NODE_ENV = 'test'
+process.env.NODE_ENV = 'test';
 
 // imports
-var restify = require('restify')
-var mongoose = require('mongoose')
-var assert = require('assert')
-var api = require('./../api')
-
-// init the test client
-var client = restify.createJsonClient({
-  url: 'http://127.0.0.1:9999',
-  version: '*'
-})
+var request = require('supertest');
+var mongoose = require('../libs/mongoose');
+var assert = require('assert');
+var app;
 
 describe('API routes testing', function () {
+  var player = {
+    username: 'api_username_1',
+    github: 'https://dummy.uri/api_username_1',
+    email: 'dummy@dummies.net',
+    registration: 1234,
+    code: 'console.log();'
+  };
 
   before(function () {
+    // starts app server
+    app = require('../api');
 
-    // Starts server
-    api.listen()
-
-    // Cleans collections
-    mongoose.connection.db.dropDatabase()
-
-  })
+    // cleans collections
+    mongoose.connection.db.dropDatabase();
+  });
 
   it('should return ok after inserting a new player.', function (done) {
+    request(app)
+      .post('/api/player')
+      .send(player)
+      .expect(200, done);
+  });
 
-    var player = {
-      username: 'api_username_1',
-      github: 'https://dummy.uri/api_username_1',
-      email: 'dummy@dummies.net',
-      registration: 1234,
-      code: 'console.log()'
-    }
+  it('should return the player inserted', function (done) {
+    request(app)
+      .get('/api/player/' + player.username)
+      .expect(200)
+      .expect(function (res) {
+        var _player = res.body.payload;
 
-    client.post('/api/player', player, function (err, req, res, data) {
-      if (err) throw err;
-      assert.equal(200, res.statusCode)
-    })
+        delete _player.__v;
+        delete _player._id;
 
-    client.get('/api/player/' + player.username, function (err, req, res, data) {
-      if (err) throw err;
-      var response = JSON.parse(res.body)
-        .payload
-      delete response.__v
-      delete response._id
-      assert.equal(200, res.statusCode)
-      assert.equal(JSON.stringify(player), JSON.stringify(response))
-    })
-
-    done()
-  })
+        assert.equal(JSON.stringify(player), JSON.stringify(_player))
+      })
+      .end(done);
+  });
 
   it('should return 204 when a player does not exists.', function (done) {
-
-    client.get('/api/player/dummy', function (err, req, res, data) {
-      assert.equal(204, res.statusCode)
-      done()
-    })
+    request(app).get('/api/player/dummy').expect(204, done);
   })
 
   it('should return BAD_REQUEST when username is different from url.', function (done) {
-
-    var player = {
-      username: 'user',
-      github: 'https://dummy.uri/api_username_1',
-      email: 'dummy@dummies.net',
-      registration: 1234,
-      code: 'console.log()'
-    }
-
-    client.put('/api/player/notuser', player, function (err, req, res, data) {
-      assert.equal(400, res.statusCode)
-      done()
-    })
-  })
+    request(app)
+      .put('/api/player/notuser')
+      .send(player)
+      .expect(400, done);
+  });
 
   after(function () {
-    mongoose.connection.db.dropDatabase()
-  })
+    app.close();
+    mongoose.connection.db.dropDatabase();
+  });
 
-})
+});
