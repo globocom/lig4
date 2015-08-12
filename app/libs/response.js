c80773130355123a93d8c08e2b974bb705ce75b9
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-08-12 00:06:40 -0300

    Removed restify true/false

--- app/libs/response.js
+++ app/libs/response.js
@@ -1,14 +1,13 @@
 'use strict'
 
 function send (statusCode, status, payload, res, next) {
-  var nextReturn = (statusCode < 400);
 
   res.status(statusCode).json({
     status: status,
     payload: payload
   });
 
-  return next(nextReturn);
+  return next();
 }
 
 module.exports.send = send;
