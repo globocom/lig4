7fbc49351ebd274a96a004cff0ac8d2d9a259fb2
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-08-19 19:32:03 -0300

    Check if code is valid

--- sandbox.js
+++ sandbox.js
@@ -3,32 +3,45 @@
 var vm = require('vm');
 var engine = require('./engine/match');
 
-function onFinish (result) {
-  process.send(result);
+function onFinish(message) {
+  console.log('Match result: ', message.result.scores)
+  process.send(message);
   process.exit();
 }
 
 process.on('message', function (match) {
 
   (function (process, module, require) {
-    process = module = require = null;
 
-    var options = { timeout: 5000 }
-    var gameContext = { Match: engine, players: {}, result: {}, id: 0 }
+    var options = {
+      timeout: 5000
+    }
+    var gameContext = {
+      Match: engine,
+      players: {},
+      result: {},
+      id: 0
+    }
     vm.createContext(gameContext);
 
     for (var player of match.players) {
       var local = {};
       vm.createContext(local);
       vm.runInContext(player.code, local, options);
-      gameContext.players[player.username] = local.Player;
+
+      if (local.Algorithm === undefined && local.Player === undefined) {
+        console.log('Invalid code for player: ', player.username)
+        process.exit()
+      }
+
+      gameContext.players[player.username] = local.Algorithm || local.Player;
       gameContext.id = match._id;
     };
 
-    // TODO: validate if p1 and p2 arent null or undefined.
     vm.createContext(gameContext);
 
-    var code = "\
+    var code =
+      "\
         var engine = new Match();                \
         for (var username in players) {          \
             var p = new players[username];       \
