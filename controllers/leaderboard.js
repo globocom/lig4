d1fcbb6e1dcb592911c14d93daea741454703f8a
Lucas Stephanou <lucas@lucas-ts.com>
2015-08-22 16:10:53 -0300

    remove leaderboard from site

--- controllers/leaderboard.js
+++ controllers/leaderboard.js
@@ -9,27 +9,28 @@ var Response = require('./../libs/response');
 
 // GET in /api/leaderboard
 router.get('/', function (req, res, next) {
-  Leaderboard
-    .find()
-    .sort({
-      score: -1,
-      win: -1,
-      gamesFor: -1,
-      gamesAgainst: 1
-    })
-    .lean()
-    .exec(function (err, leaderboards) {
-
-      if (err) return Response.send(500, 'ERRO', err, res, next);
-      if (leaderboards.length == 0) return res.sendStatus(204);
-
-      for (var leaderboard of leaderboards) {
-        delete leaderboard._id;
-        delete leaderboard.__v;
-      }
-
-      Response.send(200, 'OK', leaderboards, res, next);
-    });
+  Response.send(200, 'OK', 'Tournament is over', res, next);
+  // Leaderboard
+  //   .find()
+  //   .sort({
+  //     score: -1,
+  //     win: -1,
+  //     gamesFor: -1,
+  //     gamesAgainst: 1
+  //   })
+  //   .lean()
+  //   .exec(function (err, leaderboards) {
+  //
+  //     if (err) return Response.send(500, 'ERRO', err, res, next);
+  //     if (leaderboards.length == 0) return res.sendStatus(204);
+  //
+  //     for (var leaderboard of leaderboards) {
+  //       delete leaderboard._id;
+  //       delete leaderboard.__v;
+  //     }
+  //
+  //     Response.send(200, 'OK', leaderboards, res, next);
+  //   });
 });
 
 module.exports = router;
