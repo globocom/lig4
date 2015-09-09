e9c6def9c5731dedf89465b723e7eba59f8a10c4
Jonathan Prates <jonathan.simonprates@gmail.com>
2015-09-09 15:11:58 -0300

    Tab fix

--- test/engine/match.js
+++ test/engine/match.js
@@ -5,15 +5,19 @@ var assert = require('assert'),
 describe('Match', function () {
   it('should return null after a draw', function () {
     var match = new Match;
+
     match.addPlayer({
-        username: 'Player1',
-        klass: SequentialPlayer
-      });
+      username: 'Player1',
+      klass: SequentialPlayer
+    });
+
     match.addPlayer({
       username: 'Player2',
       klass: SequentialPlayer
     });
+
     match.run();
+
     var matchResult = match.getResults();
     assert.equal(matchResult.winner, null);
   });
