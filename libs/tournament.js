970b6fa876d39e7dc8e6f076a9dfb0f83376900d
Thiago Monaco Papageorgiou <thiago.monaco.p@gmail.com>
2015-09-18 17:31:04 -0300

    Fixing conflict issue from last merge

--- libs/tournament.js
+++ libs/tournament.js
@@ -2,14 +2,9 @@
 
 var Tournament = require('./../models/tournament');
 
-<<<<<<< HEAD
 function init() {
   return function (req, res, next) {
 
-=======
-function initialize () {
-  return function (req, res, next) {
->>>>>>> 5a0b86afff801327c150d2f566e89862038787e2
     if (req.app.get('tournament')) return next();
 
     Tournament
@@ -18,7 +13,6 @@ function initialize () {
       .equals(true)
       .exec(function (err, tournament) {
         if (err) return res.sendStatus(500);
-<<<<<<< HEAD
         if (!tournament) {
             console.log('At least one tournament should be configured.');
             return res.sendStatus(404);
@@ -28,13 +22,6 @@ function initialize () {
             return res.sendStatus(403);
         };
         req.app.set('tournament', tournament);
-=======
-        if (!tournament) return res.sendStatus(404);
-        if (!tournament.isOpen) return res.sendStatus(403);
-
-        req.app.set('tournament', tournament);
-
->>>>>>> 5a0b86afff801327c150d2f566e89862038787e2
         next();
       });
   }
