5bd27fcb91cd9f18cb4935879d2c7be3bea3b085
Thiago Monaco Papageorgiou <thiago.monaco.p@gmail.com>
2015-09-23 20:37:11 -0300

    Cron script and Makefile chage

--- Makefile
+++ Makefile
@@ -1,4 +1,4 @@
-NODE=node
+NODE=/usr/local/bin/node
 MOCHA=node_modules/mocha/bin/mocha
 KARMA=node_modules/karma/bin/karma
 BOURBON=node_modules/bourbon/app/assets/stylesheets
---
+++ worker_cron.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+set -e
+
+ROOT=/opt/puppet/current
+LOG_DIR=/tmp/lig4_worker_log/
+LOG_FILE=$LOG_DIR/`date +%Y-%m-%d-%H-%M-%S`.log
+
+export BASH_XTRACEFD=1
+set -x
+mkdir -p $LOG_DIR
+
+make -C $ROOT  worker > $LOG_FILE
