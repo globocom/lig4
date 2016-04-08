eaab7d4cc6b5d344f35ef52616a25434a0adf8c5
Jonathan Prates <jonathan.prates@corp.globo.com>
2016-04-08 17:33:18 -0300

    test: change env var name

--- test/env.js
+++ test/env.js
@@ -3,7 +3,7 @@ function env() {
 
 	process.env.NODE_ENV = 'test';
 	process.env.PORT = 6666;
-	process.env.DBAAS_MONGODB_ENDPOINT = 'mongodb://localhost:27017/test-lig4-api2';
+	process.env.MONGOLAB_URI = 'mongodb://localhost:27017/test-lig4-api2';
 	process.env.SESSION_SECRET = 'dummy';
 	process.env.GITHUB_ID = 'dummy';
 	process.env.GITHUB_SECRET = 'dummy';
